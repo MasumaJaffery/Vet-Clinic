5d8b6615959956dab577fa94d9bc6798ccd41eaf
Syeda Masuma Fatima <masumajaffery1@gmail.com>
2023-08-29 05:47:12 -0700

    Update data.sql

--- data.sql
+++ data.sql
@@ -1,5 +1,14 @@
 /* Populate database with sample data. */
-
-INSERT INTO animals (name) VALUES ('Luna');
-INSERT INTO animals (name) VALUES ('Daisy');
-INSERT INTO animals (name) VALUES ('Charlie');
+-- INSERT Queries
+-- Animal:Agumon 
+INSERT INTO animals (name, id, date_of_birth, escape_attempts, neutered, weight_kg) 
+VALUES ('Agumon', 1,'2020-2-3',0,true,10.23);
+-- Animal:Gabumon 
+INSERT INTO animals (name, id, date_of_birth, escape_attempts, neutered, weight_kg)
+VALUES ('Gabumon', 2,'2018-11-15',2,true,8.0);
+-- Animal:Pikachu
+INSERT INTO animals (name, id, date_of_birth, escape_attempts, neutered, weight_kg)
+VALUES ('Pikachu', 3,'2021-1-7',1,false,15.04);
+-- Animal:Devimon
+INSERT INTO animals (name, id, date_of_birth, escape_attempts, neutered, weight_kg)
+VALUES ('Devimon', 4,'2017-5-12',5,true,11.0);
