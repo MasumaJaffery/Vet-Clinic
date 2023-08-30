f30ec6557142493a7733654d730c5b9b75a6913c
Syeda Masuma Fatima <masumajaffery1@gmail.com>
2023-08-30 11:23:46 -0700

    Insert Queries

--- data.sql
+++ data.sql
@@ -12,3 +12,24 @@ VALUES ('Pikachu', 3,'2021-1-7',1,false,15.04);
 -- Animal:Devimon
 INSERT INTO animals (name, id, date_of_birth, escape_attempts, neutered, weight_kg)
 VALUES ('Devimon', 4,'2017-5-12',5,true,11.0);
+-- Animal:Charmander
+INSERT INTO animals (name, id, date_of_birth, escape_attempts, neutered, weight_kg)
+VALUES ('Charmander', 5,'2020-2-8',0,false,11.0);
+-- Animal:Plantmon
+INSERT INTO animals (name, id, date_of_birth, escape_attempts, neutered, weight_kg)
+VALUES ('Plantmon', 6,'2021-11-15',2,true,5.7);
+-- Animal:Squirtle
+INSERT INTO animals (name, id, date_of_birth, escape_attempts, neutered, weight_kg)
+VALUES ('Squirtle', 7,'1993-4-2',3,false,12.13);
+-- Animal:Angemon
+INSERT INTO animals (name, id, date_of_birth, escape_attempts, neutered, weight_kg)
+VALUES ('Angemon', 8,'2005-6-12',1,true,45.0);
+-- Animal:Boarmon
+INSERT INTO animals (name, id, date_of_birth, escape_attempts, neutered, weight_kg)
+VALUES ('Boarmon', 9,'2005-6-7',7,true,20.4);
+-- Animal:Blossom
+INSERT INTO animals (name, id, date_of_birth, escape_attempts, neutered, weight_kg)
+VALUES ('Blossom', 10,'1998-10-13',3,true,17.0);
+-- Animal:Ditto
+INSERT INTO animals (name, id, date_of_birth, escape_attempts, neutered, weight_kg)
+VALUES ('Ditto', 11,'2022-5-14',4,true,22.0);
