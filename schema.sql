7db8c6ab6be9009c26360137cd88acadf421cf9d
Grandi0z <josephlkhausi@gmail.com>
2023-09-04 23:27:30 +0200

    Decrease the execution time

--- schema.sql
+++ schema.sql
@@ -49,3 +49,8 @@ CREATE TABLE visits(
 );
 
 ALTER TABLE owners ADD COLUMN email VARCHAR(120);
+
+CREATE INDEX idx_animal_id ON visits(animal_id);
+CREATE INDEX idx_vet_id ON visits(vet_id);
+CREATE INDEX idx_email ON owners(email);
+CLUSTER visits USING idx_animal_id;
