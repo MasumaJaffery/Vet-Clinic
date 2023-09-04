5d1662344e11af82beca85a976f103e99e61351a
Syeda Masuma Fatima <masumajaffery1@gmail.com>
2023-09-04 16:33:25 -0700

    Commit

--- queries.sql
+++ queries.sql
@@ -1,1 +1,8 @@
-explain analyze SELECT COUNT(*) FROM visits where animal_id = 4
+SELECT COUNT(*) FROM visits where animal_id = 4;
+EXPLAIN ANALYZE SELECT COUNT(*) FROM visits where animal_id = 4;
+
+SELECT * FROM owners WHERE email = 'owner_18327@mail.com';
+EXPLAIN ANALYZE SELECT * FROM owners WHERE email = 'owner_18327@mail.com';
+
+SELECT * FROM visits where vet_id = 2;
+EXPLAIN ANALYZE SELECT * FROM visits WHERE vet_id = 2;
